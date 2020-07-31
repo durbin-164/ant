fa761ae092432d673216c9a501329068ad9b7748
Md. Masud Rana <masud.rana@infolytx.com>
2020-07-31 22:08:15 +0600

    refactor build wrapper

--- script/sonar_build_wrapper.sh
+++ script/sonar_build_wrapper.sh
@@ -2,6 +2,6 @@ wget -q https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -q build-wrapper-linux-x86.zip
 rm -r build-wrapper-linux-x86.zip
 
-./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build build/temp*
+./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output python setup.py test
 
-rm -r build-wrapper-linux-x86
+# rm -r build-wrapper-linux-x86
