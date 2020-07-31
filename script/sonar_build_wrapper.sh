84498a3b1498d12b8962a995390132f79f5557ad
Md. Masud Rana <masud.rana@infolytx.com>
2020-08-01 01:20:43 +0600

    update sonar

--- script/sonar_build_wrapper.sh
+++ script/sonar_build_wrapper.sh
@@ -1,12 +1,13 @@
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+
 wget -q https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -q build-wrapper-linux-x86.zip
 rm -r build-wrapper-linux-x86.zip
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
-cd ..
 
-./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build build/
+
+./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw_output make clean all
 
 # rm -r build-wrapper-linux-x86
