a212fe35b5e12b0634eed02a6f775d701a98db78
Md. Masud Rana <masud.rana@infolytx.com>
2020-07-31 23:20:30 +0600

    update sonar build

--- script/sonar_build_wrapper.sh
+++ script/sonar_build_wrapper.sh
@@ -2,6 +2,6 @@ wget -q https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -q build-wrapper-linux-x86.zip
 rm -r build-wrapper-linux-x86.zip
 
-./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output python setup.py test
+./build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output python setup.py build_ext
 
 # rm -r build-wrapper-linux-x86
