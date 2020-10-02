148310a625ae0620ef4189209fc9a67dfaa00ab6
Md. Masud Rana <masud.rana@infolytx.com>
2020-10-02 15:07:49 +0600

    init commit

---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include<iostream>
+
+void Hello::sayHello(std::string name){
+    std::cout<<"hello "<<name<<" !!!"<<std::endl;
+}
---
+++ src/hello.h
@@ -0,0 +1,14 @@
+#include<string>
+
+///Hello Class
+class Hello
+{
+
+    public:
+        /// say hello to audient
+        /// @param name to say hello
+        void sayHello(std::string name);
+};
+
+
+
