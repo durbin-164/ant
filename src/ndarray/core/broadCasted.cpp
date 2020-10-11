ab00746e166580096e610c6876ae9ca787e73def
Md. Masud Rana <masud.rana@infolytx.com>
2020-10-11 14:44:45 +0600

    add broadcasted

--- src/ndarray/core/broadCasted.cpp
+++ src/ndarray/core/broadCasted.cpp
@@ -6,21 +6,6 @@
 namespace ndarray
 {
 
-struct BroadCastedProperty{
-    Shape a_shape;
-    Shape b_shape;
-    Stride a_stride;
-    Stride b_stride;
-
-    BroadCastedProperty(Shape &a_shape_, Shape &b_shape_, Stride &a_stride_, Stride &b_stride_ ){
-        a_shape = a_shape_;
-        b_shape = b_shape_;
-        a_stride = a_stride_;
-        b_stride = b_stride_;
-    }
-};
-
-
 Shape getBroadCastedShape(const Shape &l_shape, const Shape &r_shape,const int offset){
     if(l_shape.size()==0 || (l_shape.size()==1 && l_shape[0]==0) ||
        r_shape.size()==0 || (r_shape.size()==1 && r_shape[0]==0)){
@@ -32,7 +17,7 @@ Shape getBroadCastedShape(const Shape &l_shape, const Shape &r_shape,const int o
     }
 
     if(l_shape.size()<r_shape.size()){
-        return getBroadCastedShape(r_shape, l_shape);
+        return getBroadCastedShape(r_shape, l_shape, offset);
     }
 
 
@@ -66,5 +51,51 @@ Shape getBroadCastedShape(const Shape &l_shape, const Shape &r_shape,const int o
     return out_shape;
 }
 
+BroadCastedProperty getBroadCastedProperty(const ndarray::Shape &out_shape, const ndarray::Array &A, const ndarray::Array &B,const int offset){
+    ndarray::Shape l_shape = ndarray::paddedVector(A.shape(), out_shape.size(), 1);
+    ndarray::Shape r_shape = ndarray::paddedVector(B.shape(), out_shape.size(), 1);
+
+    ndarray::Stride l_stride = ndarray::paddedVector(A.stride(), out_shape.size(), 0);
+    ndarray::Stride r_stride = ndarray::paddedVector(B.stride(), out_shape.size(), 0);
+    
+
+    ndarray::Stride broad_l_stride;
+    ndarray::Stride broad_r_stride;
+
+    //keep same stride between (last index to last-offset index).
+    int unchanged_stride_index = out_shape.size()-offset;
+
+    for(int i=0;i<out_shape.size();i++){
+        int l = l_shape[i];
+        int r = r_shape[i];
+
+        int ls = (l==r || r ==1 || i>=unchanged_stride_index) ? l_stride[i] : 0;
+        int rs = (l==r || l ==1 || i>=unchanged_stride_index) ? r_stride[i] : 0;
+
+        broad_l_stride.push_back(ls);
+        broad_r_stride.push_back(rs);
+    } 
+    
+    return BroadCastedProperty(l_shape, r_shape, broad_l_stride, broad_r_stride);
+}
+
+
+std::vector<int>paddedVector(const std::vector<int>&vec, const int size, const int pad_value){
+
+    if(size<vec.size()){
+        std::stringstream ss;
+        ss <<"invalid padded size where expected size "<<size;
+        ss<<" is less then give data size "<<vec.size()<<".";
+        throw std::runtime_error(ss.str());
+    }
+
+    int pad_size = size- vec.size();
+    std::vector<int>out_vec;
+    out_vec.reserve(size);
+    out_vec.resize(pad_size, pad_value);
+
+    out_vec.insert(out_vec.end(), vec.begin(), vec.end());
+    return out_vec;
+}
 
 }//end ndarray namespace
--- src/ndarray/core/broadCasted.h
+++ src/ndarray/core/broadCasted.h
@@ -6,9 +6,23 @@ namespace ndarray
 {
 
 struct BroadCastedProperty;
-
 Shape getBroadCastedShape(const Shape &l_shape, const Shape &r_shape, const int offset = 0);
 
-BroadCastedProperty getBroadCastedProperty(Shape out_shape, const ndarray::Array &A, const ndarray::Array &B,const int offset = 0);
+BroadCastedProperty getBroadCastedProperty(const ndarray::Shape &out_shape, const ndarray::Array &A, const ndarray::Array &B,const int offset = 0);
+std::vector<int>paddedVector(const std::vector<int>&vec, const int size, const int pad_value = 0);
+
+struct BroadCastedProperty{
+    Shape a_shape;
+    Shape b_shape;
+    Stride a_stride;
+    Stride b_stride;
+
+    BroadCastedProperty(Shape &a_shape_, Shape &b_shape_, Stride &a_stride_, Stride &b_stride_ ){
+        a_shape = a_shape_;
+        b_shape = b_shape_;
+        a_stride = a_stride_;
+        b_stride = b_stride_;
+    }
+};
 
 }// need ndarray name space
